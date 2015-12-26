687e7c91f3695b62d6c13a4cce16bea770277989
Will Riley <wanderingwill@gmail.com>
2015-12-26 01:33:04 -0500

    Added additional prediction functions.

--- admissionprobability.js
+++ admissionprobability.js
@@ -4,7 +4,7 @@ function probTwo(prA, prB) {
 
 function probArray(prArray) {
 	if (!prArray || prArray.length === 0) {
-		return undefined;
+		return 0;
   } else if (prArray.length === 1) {
 		return prArray[0];
 	} else {
@@ -20,6 +20,50 @@ function getProbabilityOfAdmissionToAtLeastOneSchool(schoolAdmissionProbArray) {
 	return probArray(schoolAdmissionProbArray);
 }
 
+function getMinSchoolCountForConstantAdmissionProbability(constantProb, minProb) {
+	var i = 1;
+	var lastProb = constantProb;
+	while (lastProb < minProb) {
+		lastProb = probTwo(lastProb, constantProb);
+		i++;
+	}
+	return i;
+}
+
+function getMinConstantAdmissionProbabilityForSchoolCount(schoolCount, minProbAdmisAtLeastOneSchool, allowedError) {
+	var getProbs = function(p) {
+		var probs = [];
+		for(var i = 0; i < schoolCount; i++) {
+			probs.push(p);
+		}
+		return probs;
+	}
+	if (minProbAdmisAtLeastOneSchool === undefined) {
+		minProbAdmisAtLeastOneSchool = .99;
+	}
+	if (allowedError === undefined) {
+		allowedError = .001;		
+	}
+	var lowerProb = 0;
+	var higherProb = 1;
+	var lastConstantProb = .5;
+	var lastProbs = getProbs(lastConstantProb);
+	var probDiff = getProbabilityOfAdmissionToAtLeastOneSchool(lastProbs) - minProbAdmisAtLeastOneSchool;
+	while(probDiff < 0 || probDiff > allowedError) {
+		if (probDiff > allowedError) {
+			higherProb = lastConstantProb;
+		} else {
+			lowerProb = lastConstantProb;
+		}
+		lastConstantProb = (lowerProb + higherProb) / 2.0;
+		lastProbs = getProbs(lastConstantProb);
+		probDiff = getProbabilityOfAdmissionToAtLeastOneSchool(lastProbs) - minProbAdmisAtLeastOneSchool;
+	}
+	return lastConstantProb;
+}
+
 module.exports = {
 	getProbabilityOfAdmissionToAtLeastOneSchool: getProbabilityOfAdmissionToAtLeastOneSchool,
+	getMinSchoolCountForConstantAdmissionProbability: getMinSchoolCountForConstantAdmissionProbability,
+	getMinConstantAdmissionProbabilityForSchoolCount: getMinConstantAdmissionProbabilityForSchoolCount
 };
